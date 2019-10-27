517b8dd8dcd2ec5cd230161477e79cb927a73867
smv5047 <smv5047@gmail.com>
2019-10-27 19:29:23 -0400

    index file setup for Redux

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -3,4 +3,24 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+//Redux Imports
+import { Provider } from "react-redux"
+import { createStore, applyMiddleware, combineReducers } from "redux"
+
+//Middleware Imports
+import logger from "redux-logger"
+import thunk from "redux-thunk"
+
+
+const rootReducer = combineReducers({
+
+})
+
+
+const store = createStore(rootReducer, applyMiddleware(thunk, logger))
+
+ReactDOM.render(
+<Provider store={store}>
+    <App />
+</Provider>
+, document.getElementById("root"));
